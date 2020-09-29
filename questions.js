1f96b5f9d6702103f6acbbfa04860570a138212c
Kelsea Fields <kelsea.fields@yahoo.com>
2020-09-28 22:14:42 -0700

    continued changes. struggling with a few details

---
+++ questions.js
@@ -0,0 +1,38 @@
+var question = [
+  {
+    questionTitle: " What does NaN represent?",
+    answerChoices: [
+      "Not A Number",
+      "Nick At Nite",
+      "Niche Area Network",
+      "Not A Name",
+    ],
+    correctAnswer: "Not A Number",
+  },
+  {
+    questionTitle: "What Kind of Language is JavacSript?",
+    answerChoices: ["Programming", "Scripting", "Both A & B", "Browser"],
+    correctAnswer: "Scripting",
+  },
+  {
+    questionTitle: "What Are The Two Boolean Variables?",
+    answerChoices: [
+      "Yes and No",
+      "Please and Thank You",
+      "True and False",
+      "Up and Down",
+    ],
+    correctAnswer: "True and False",
+  },
+  {
+    questionTitle: " Which <tag> Is A JavaScript File Stored In a HTML file?",
+    answerChoices: ["<p>", "<style>", "<html>", "<script>"],
+    correctAnswer: "<script>",
+  },
+  {
+    questionTitle:
+      "How Would You Check For Special Circumstances? ______ statement.",
+    answerChoices: ["For", "Let", "While", "If"],
+    correctAnswer: "If",
+  },
+];
--- script.js
+++ script.js
@@ -1,77 +1,98 @@
 var startButtonEl = document.querySelector("#startButton");
-// need help with my start button event listener
-console.log("click", startButtonEl);
-
 var nextButton = document.querySelector("#nextButton");
 var countDownEl = document.querySelector("#timer");
-var buttonAEl = document.querySelector(".buttonA");
-var buttonBEl = document.querySelector(".buttonB");
-var buttonCEl = document.querySelector(".buttonC");
-var buttonDEl = document.querySelector(".buttonD");
-
+var questionScreen = document.querySelector("#question");
+var questionTitleH2 = document.querySelector("#questionTitle");
+var answerChoicesDiv = document.querySelector("#answerChoices");
+var correctAnswerDiv = document.querySelector("#correctAnswer");
+var pTag = document.querySelectorAll(".pTag");
+var endPageDiv = document.querySelector("#endPage");
+var startScreenID = document.querySelector("#startScreen");
+console.log("start", startButtonEl);
 console.log("buttons", nextButton);
 console.log("buttons", countDownEl);
-console.log("buttons", buttonAEl);
-console.log("buttons", buttonBEl);
-console.log("buttons", buttonCEl);
-console.log("buttons", buttonDEl);
+console.log("questionArrHTML", questionDiv);
+console.log("questionTitles", questionTitleH2);
+console.log("answer choices", answerChoicesDiv);
+console.log("correctAnswer", correctAnswerDiv);
+console.log("ptag", pTag);
+console.log("end game", endPageDiv);
+console.log("startscreen", startScreenID);
 
-var questionTitle = [
-  "create question here",
-  "Here's another question",
-  "another question",
-  "last question",
-];
-console.log(questionTitle.length);
+var questionDiv = [questionTitleH2, answerChoicesDiv, correctAnswerDiv];
+console.log(questionDiv);
 
-var multipleChoice = ["a", "b", "c", "d"];
-var correctAnswer = ["a", "b", "c", "d"];
-var aAnswers = ["", "", "", ""];
-var bAnswers = ["hi", "", "", ""];
-var cAnswers = ["", "", "", ""];
-var dAnswers = ["", "", "", ""];
+var questionScreen = questionDiv;
+console.log("hi", questionScreen);
 
-console.log(aAnswers);
-console.log(bAnswers[0]);
-console.log(cAnswers);
-console.log(dAnswers);
+// var questionTitle = [
+//   "create question here",
+//   "Here's another question",
+//   "another question",
+//   "last question",
+// ];
+// console.log(questionTitle.length);
 
-quizDisplay = [
-  questionTitle,
-  aAnswers,
-  bAnswers,
-  cAnswers,
-  dAnswers,
-  correctAnswer,
-];
-console.log(quizDisplay);
-console.log(Object);
-var questionTitleAsks = questionTitle.length;
-console.log(questionTitle[1]);
+// var multipleChoice = ["a", "b", "c", "d"];
+// var correctAnswer = ["a", "b", "c", "d"];
+// var aAnswers = ["", "", "", ""];
+// var bAnswers = ["hi", "", "", ""];
+// var cAnswers = ["", "", "", ""];
+// var dAnswers = ["", "", "", ""];
 
+// console.log(aAnswers);
+// console.log(bAnswers[0]);
+// console.log(cAnswers);
+// console.log(dAnswers);
+
+// quizDisplay = [
+//   questionTitle,
+//   aAnswers,
+//   bAnswers,
+//   cAnswers,
+//   dAnswers,
+//   correctAnswer,
+// ];
+// console.log(quizDisplay);
+// console.log(Object);
+// var questionTitleAsks = questionTitle.length;
+// console.log(questionTitle[1]);
+
+// countDownEl.appendChild;
 function startingClock() {
   var timeOnClock = 50;
-
+  timeOnClock--;
   var timeInterval = setInterval(function () {
-    countDownEl.textContent = timeOnClock + "seconds remaining!";
     if (((i = 0), i > timeOnClock.length)) {
-      timeOnClock--;
-    } else if (timeOnClock > 50) {
+    } else {
+      timeOnClock > 50;
       clearInterval(timeInterval);
-      goTime();
+      // startQuiz();
     }
+    countDownEl.textContent = timeOnClock + "seconds remaining!";
     console.log("time", timeOnClock);
   }, 1000);
 }
-// timer is running but not backwards and not showing
-function goTime() {
-  var i = 0;
-  console.log("start timer");
-}
-parseInt(startingClock);
+var mode = "hide";
+// var quiz = "questions";
 
-//  hide next button only one with hide property?
-startingClock();
+startButtonEl.addEventListener("click", function () {
+  console.log("start button is clicked");
+
+  startScreenID.setAttribute("class", "hide");
+  console.log("hiding", startScreenID);
+
+  // questionScreen.setAttribute("class", "questions");
+  // console.log("question screen showing", questionScreen);
+
+  // questionDiv.("class", "hide");
+  console.log("showing!", questionDiv);
+  startingClock();
+});
+
+nextButton.addEventListener("click", function () {
+  console.log("next button is clicked");
+});
 // when start button is clicked
 // hide start button, ptag,
 // unhide next button, a,b,c,d buttons, h2
@@ -82,7 +103,6 @@ startingClock();
 // display correct answer
 // store begin quiz into an array of answers
 // usersAnswers = store elsewhere?
-// function nextButton() {
 //   console.log("nexted");
 // display questions array[1]
 // answer is selected
@@ -96,5 +116,4 @@ startingClock();
 //   // will need something to end entire game
 //   // and bring up the end page where you can enter your high score
 //   // console.log("number" + setTimer)
-// // function userAnswer() {
-// //   console.lo(userAnswer);
+// // function userAnswer()
